e008530db57637329f06448bb8f55a7944ad6b94
Nik Cherednik <ighosta9@gmail.com>
2025-03-14 18:47:13 +0300

    test: add unit tests for defaultParser functionality

--- libs/parsers/src/lib/parsers.spec.ts
+++ libs/parsers/src/lib/parsers.spec.ts
@@ -1,7 +1,50 @@
 import { parsers } from './parsers';
+import { defaultParser } from '../index';
 
 describe('parsers', () => {
   it('should work', () => {
     expect(parsers()).toEqual('parsers');
+  });
+});
+
+describe('defaultParser', () => {
+  it('should parse a single number', () => {
+    expect(defaultParser('42')).toEqual([42]);
+  });
+
+  it('should parse multiple numbers', () => {
+    expect(defaultParser('1 2 3 4 5')).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('should handle negative numbers', () => {
+    expect(defaultParser('-1 -2 3 -4')).toEqual([-1, -2, 3, -4]);
+  });
+
+  it('should handle decimal numbers', () => {
+    expect(defaultParser('1.5 2.7 -3.2')).toEqual([1.5, 2.7, -3.2]);
+  });
+
+  it('should handle leading/trailing whitespace', () => {
+    expect(defaultParser('  1 2 3  ')).toEqual([1, 2, 3]);
+  });
+
+  it('should handle multiple spaces between numbers', () => {
+    expect(defaultParser('1   2     3')).toEqual([1, 2, 3]);
+  });
+
+  it('should handle empty string', () => {
+    expect(defaultParser('')).toEqual([]);
+  });
+
+  it('should handle string with only whitespace', () => {
+    expect(defaultParser('   ')).toEqual([]);
+  });
+
+  it('should handle invalid number strings', () => {
+    expect(defaultParser('1 abc 2')).toEqual([1, NaN, 2]);
+  });
+
+  it('should handle zero', () => {
+    expect(defaultParser('0 -0 0.0')).toEqual([0, -0, 0]);
   });
 });
